82da41dd60aa1920d77ce84600f3848fbba4e252
zhourui <zhourui@agora.io>
2023-12-13 01:35:03 +0800

    publish 1.0.0

---
+++ A/Targets/Target_A.h
@@ -0,0 +1,18 @@
+//
+//  Target_A.h
+//  A
+//
+//  Created by ZhouRui on 2023/12/13.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface Target_A : NSObject
+
+- (UIViewController *)Action_viewController:(NSDictionary *)params;
+
+@end
+
+NS_ASSUME_NONNULL_END
