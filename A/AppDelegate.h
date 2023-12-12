ef5920e69cc2c2744f493695709b8e0f42977b6d
zhourui <zhourui@agora.io>
2023-12-12 23:56:05 +0800

    Initial Commit

---
+++ A/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  A
+//
+//  Created by ZhouRui on 2023/12/12.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ A/ViewController.m
@@ -0,0 +1,22 @@
+//
+//  ViewController.m
+//  A
+//
+//  Created by ZhouRui on 2023/12/12.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end
